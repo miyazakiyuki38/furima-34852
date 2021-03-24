14925e247799f19ef03985160569af987bf681f9
miyazakiyuki38 <79497965+miyazakiyuki38@users.noreply.github.com>
2021-03-24 11:11:24 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :family_name, presence: true
+         validates :family_name_kana, presence: true
+         validates :first_name, presence: true
+         validates :first_name_kana, presence: true
+         validates :birth_day,presence: true
+
+         has_many :items
+         has_many :orders
+
+         validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+
 end
