cf4771593343c16d703aed6b341aeb04bf4929c9
miyazakiyuki38 <79497965+miyazakiyuki38@users.noreply.github.com>
2021-03-24 16:37:54 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :family_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-         validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Namekana kana Full-width katakana characters"}
-         validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Namekana kana Full-width katakana characters"}
-         validates :birth_day,presence: true
-        #  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
-         validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :family_name_kana, presence: true,
+                               format: { with: /\A[ァ-ヶー－]+\z/, message: 'Namekana kana Full-width katakana characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー－]+\z/, message: 'Namekana kana Full-width katakana characters' }
+  validates :birth_day, presence: true
+  #  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
 
-         has_many :items
-         has_many :orders
-
+  has_many :items
+  has_many :orders
 end
