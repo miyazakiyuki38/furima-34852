5e43257787db19d2edd0286199028de0cf29ab41
miyazakiyuki38 <79497965+miyazakiyuki38@users.noreply.github.com>
2021-03-27 16:20:25 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except:[:index,:show]
+
   def index
      @items = Item.order("crateed_at DESC")
   end
 
   def new
     @item = Item.new
-  end
+ end
+
 
 def create
   @item = Item.new(item_params)
